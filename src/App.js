28991574f3adc3df5286770f4b11dfe9cf139905
Antonio <antoniodaria@msn.com>
2020-10-02 17:57:38 +0100

    Added app basic router and snapshot test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
-import { Button } from "antd";
 import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+export default function App() {
 
-export default App;
+  function Home() {
+    return <h2>Home Page</h2>;
+  }
+  
+  function About() {
+    return <h2>About Page</h2>;
+  }
+    
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home /> 
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+
