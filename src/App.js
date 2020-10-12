751c1ab8be30b2515eed4913bce732b52eff2191
Nick Morgan <nicholas.morgan@ibm.com>
2020-10-12 20:01:32 +0100

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/homePage";
