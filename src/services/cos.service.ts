e9fee0dba0a0c999433e96c50f171a94bf58d44e
Tom Madelin + Matt Ocovsky <tom.madelin@gmail.com>
2021-01-07 22:38:38 +0000

    finnish cos service

--- src/services/cos.service.ts
+++ src/services/cos.service.ts
@@ -1,8 +1,10 @@
 import { CosCredentials } from "../types/Cos";
 import { getCosCredentials } from "../util/cosCreds";
-import { Service } from "typedi";
+import { Inject, Service } from "typedi";
 import {S3} from "ibm-cos-sdk"
 import { COS_FLASHCARD_IMAGE_BUCKET } from "../statics";
+import { LoggerApi } from "../logger";
+import { InternalServerError } from "routing-controllers";
 
 /*
   This service is used internally to provide cos object operations to other services
@@ -10,23 +12,32 @@ import { COS_FLASHCARD_IMAGE_BUCKET } from "../statics";
 */
 @Service()
 export class CosService {
+    private logger: LoggerApi;
     private cos: S3
     private creds: CosCredentials
 
-    constructor() {
+    constructor(
+        @Inject("logger")
+        logger: LoggerApi
+    ) {
+        this.logger = logger.child("CosService")
         this.creds = getCosCredentials();
         this.cos = new S3(this.creds);
     }
 
-    getObjects() {
-        const params = {
-            Bucket: COS_FLASHCARD_IMAGE_BUCKET,
-            Key: this.creds.apiKeyId
-        }
-
-        return this.cos.getObject(params, function(err, data) {c
-            if (err) console.log(err, err.stack); // an error occurred
-            else     console.log(data);           // successful response
-          });
+    async getFlashcardImage(imageId: string): Promise<S3.GetObjectOutput> {
+        try {
+            console.log("image", imageId)
+            const cosImage = await this.cos
+              .getObject({ Key: imageId, Bucket: COS_FLASHCARD_IMAGE_BUCKET })
+              .promise();
+      
+            return cosImage;
+          } catch (error) {
+            this.logger.error(error);
+            throw new InternalServerError(
+              "Retrieving images from Cloud Object Store has failed"
+            );
+          }
     }
 }
