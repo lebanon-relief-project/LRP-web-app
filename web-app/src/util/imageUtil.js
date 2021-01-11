c95388d758bae5a5583bad1b31cb5bfd91103eda
Tom Madelin + Matt Ocovsky <tom.madelin@gmail.com>
2021-01-11 15:45:33 +0000

    add COS to manifest + update getSvgImageFromArrayBuffer

--- web-app/src/util/imageUtil.js
+++ web-app/src/util/imageUtil.js
@@ -1,8 +1,6 @@
 // Converts an ArrayBuffer to image data to be used in a <img />
 export const getSvgImageFromArrayBuffer = (bufferData) => {
-  const data = `data:image/svg+xml;base64,${new Buffer(bufferData).toString(
-    "base64"
-  )}`;
-
-  return data;
+  return URL.createObjectURL(
+    new Blob([new Buffer(bufferData)], { type: "image/svg+xml" })
+  );
 };
