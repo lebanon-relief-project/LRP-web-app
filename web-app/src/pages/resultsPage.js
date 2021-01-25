dcf80f6029cf1ee5de5646a8807397e32b0dbf0b
matejoslav <matejoslav@gmail.com>
2021-01-25 22:54:36 +0000

    call results service on results page

--- web-app/src/pages/resultsPage.js
+++ web-app/src/pages/resultsPage.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import devices from "../styles/Devices";
 import ExperiencesSection from "../components/resultsPage/ExperiencesSection";
 import ResultsSection from "../components/resultsPage/ResultsSection";
+import { getCardIdsFromSessionStorage } from "../util/util";
+import { getResults } from "../services/results.service";
 
 const ResultsPage = () => {
+  const [results, setResults] = useState(undefined);
+  const fetchResults = async () => {
+    let ids = getCardIdsFromSessionStorage();
+    let response = await getResults(ids);
+
+    setResults(response ? response.results : undefined);
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
   return (
     <StyledPage>
       <div style={{ width: "100%" }}>
--- web-app/src/pages/resultsPage.spec.js
+++ web-app/src/pages/resultsPage.spec.js
@@ -2,11 +2,22 @@ import React from "react";
 import { renderWithRouter } from "../util/testUtils";
 import ResultsPage from "./resultsPage";
 
+import * as resultsService from "../services/results.service.js";
+import * as util from "../util/util.js";
+import { wait } from "@testing-library/react";
+
 jest.mock("../services/results.service.js");
+jest.mock("../util/util.js");
 
 jest.mock("../components/resultsPage/ExperiencesSection", () => {
   return () => <div>Experiences Section</div>;
 });
+
+const getResultsSpy = jest.spyOn(resultsService, "getResults");
+const getCardIdsFromSessionStorageSpy = jest.spyOn(
+  util,
+  "getCardIdsFromSessionStorage"
+);
 
 describe("the ResultsPage component", () => {
   beforeEach(() => {
@@ -15,12 +26,36 @@ describe("the ResultsPage component", () => {
     sessionStorage.setItem("key2", "value2");
   });
 
-  it("should match the snapshot", () => {
+  it("should match the snapshot", async () => {
     const { container } = renderWithRouter(<ResultsPage />);
+    await wait();
     expect(container).toMatchSnapshot();
   });
 
-  it.todo(
-    "should call the results service with card id's that are in session storage"
-  );
+  it("should call the results service with card id's that are in session storage", async () => {
+    getCardIdsFromSessionStorageSpy.mockReturnValue(["key1", "key2"]);
+    getResultsSpy.mockReturnValue({
+      results: [
+        {
+          _id: "id",
+          _rev: "rev",
+          expl_title: "title",
+          expl_body: "body",
+          image: "image",
+          recommendations: [
+            {
+              title: "some recommendation title",
+              body: "some recommendation body",
+            },
+          ],
+        },
+      ],
+    });
+    const { container } = renderWithRouter(<ResultsPage />);
+
+    await wait();
+
+    expect(getCardIdsFromSessionStorageSpy).toHaveBeenCalled();
+    expect(getResultsSpy).toHaveBeenCalledWith(["key1", "key2"]);
+  });
 });
