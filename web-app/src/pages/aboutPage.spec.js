d9e243b84f8658fd2146e861d1ca0bdf3bb6d014
matejoslav <matejoslav@gmail.com>
2021-01-30 17:45:23 +0000

    fixed snapshot tests

--- web-app/src/pages/aboutPage.spec.js
+++ web-app/src/pages/aboutPage.spec.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import AboutPage from "./aboutPage";
+import "jest-styled-components";
 
 describe("the AboutPage component", () => {
   it("should match the snapshot", () => {
