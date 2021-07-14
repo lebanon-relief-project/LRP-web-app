8083c7f2b98a5b19e0ad3a01006dd19927f91d8b
nicholasmorgan <58466634+nicholasmorgan@users.noreply.github.com>
2021-07-14 13:04:57 +0100

    changed filed types (#142)

--- src/types/Psychotherapist.ts
+++ src/types/Psychotherapist.ts
@@ -7,18 +7,18 @@ interface Psychotherapist {
   location: string;
   therapyServices: { [theraphyService: string]: boolean };
   licenseNumber: string;
-  emergencyPhoneNumber?: number;
+  emergencyPhoneNumber?: string;
   consentData: ConsentData;
   website: string;
   email: string;
   patientGroups: { [patientGroup: string]: boolean };
   supportedGenders: { [supportedGender: string]: boolean };
   legalPersonality: string;
-  phoneNumber: number;
+  phoneNumber: string;
   picture: string;
   therapistGender: string;
   languages: string[];
-  internationalPaymentsOnly: string;
-  freeService: string;
+  internationalPaymentsOnly: boolean;
+  freeService: boolean;
   bookingApp: string;
 }
